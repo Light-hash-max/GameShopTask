5fe287c74ab74f7483f48030de5bdef225ffd648
Light-hash-max <44718776+Light-hash-max@users.noreply.github.com>
2021-09-20 08:52:18 +0700

    Add files via upload

---
+++ Assets/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int cost;
+
+    public void Buy()
+    {
+        if (GetComponentInParent<Shop>().money >= cost)
+        {
+            GetComponentInParent<Shop>().money -= cost;
+        }
+    }
+}
---
+++ Assets/Shop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    public int money = 100;
+    public Text moneyText;
+
+    public void buyItem(string item)
+    {
+        moneyText.text = money.ToString();
+    }
+}
